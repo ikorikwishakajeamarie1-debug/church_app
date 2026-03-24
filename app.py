1d293e07bf28f925c24e9628513c606b8940c0eb
jean Marie Ikorikwishaka <ikorikwishakajeamarie1@gmail.com>
2026-03-24 07:29:24 -0700

    fix host and port for Render

--- app.py
+++ app.py
@@ -107,5 +107,7 @@ with app.app_context():
 
 
 # ================= RUN APP =================
-if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=False)
