92847e981a4a3b80e7a41d68d950ec9aafb83ef3
jean Marie Ikorikwishaka <ikorikwishakajeamarie1@gmail.com>
2026-03-26 08:24:32 -0700

    Add member form and save functionality

--- app.py
+++ app.py
@@ -159,6 +159,10 @@ def update_member(id):
         print("Error updating member:", e)
         return "❌ Internal Server Error — check console/logs"
     return redirect('/members')
-
+@app.route("/members")
+def members():
+    cursor.execute("SELECT * FROM members")
+    all_members = cursor.fetchall()
+    return render_template("members.html", members=all_members)
 if __name__ == "__main__":
     app.run(debug=True)
